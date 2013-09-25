030847eaa7fb39d75959f1d5f15df5b2b6d1bdaa
ifanchu <ifanchu@IFANs-iMac.local>
2013-09-24 18:12:36 -0700

    README.md updated

--- ICNode/ICNode.h
+++ ICNode/ICNode.h
@@ -13,30 +13,36 @@ objectInChildrenAtIndex:
 firstObject
 lastObject
 count
- 
+
  Assumptions:
  1. All the index mentioned in below methods is relative to the current ICNode.
  2. For any traversal operation, use DFS pre-order traversal to mimic array functionality
- 
+ 3. Any adding and moving should not affect the existing order. For example, moving a node to its sibling means move that node to be as its sibling's last child.
+
 */
+
 @interface ICNode : NSObject
 {
 
 }
 #pragma mark - properties
-// the object that represents the data
-@property (nonatomic, copy) id data;
+// the object that represents the data stored in this node
+@property (nonatomic, strong) id data;
 // the ICNode which is the parent of this node
-@property (nonatomic, weak) ICNode *parent;
+@property (nonatomic, strong) ICNode *parent;
 // an array of ICNode, the reason to use an NSArray is that we need the ordering of NSArray
 @property (nonatomic, strong) NSMutableArray *children;
+// An integer which indicates the index of this node in its parent's children array
+@property (nonatomic, strong) NSInteger indexOfParent;
 
 #pragma mark - Initializers
 - (id)initWithData:(id)aData withParent:(ICNode *)aParent;
+- (id)initAsRootNode;
 
 #pragma mark - Querying the tree
 /*
- Return the root ICNode of this ICNode if any
+ Return the root ICNode of this ICNode.
+ If parent is nil, return this node; recursively find the parent until parent is nil.
  */
 - (ICNode *)getRootNode;
 /*
@@ -54,29 +60,42 @@ count
 /*
  Return whether this ICNode has no child
  */
-- (BOOL)noChild;
+- (BOOL)isLeaf;
+/*
+ The depth of this node starting from root.
+ */
+// @return The depth of this node; 0 if this node is root
+- (NSInteger)depth;
+- (BOOL) hasNextSibling;
+- (BOOL) hasPreviousSibling;
 #pragma mark - Finding objects
 /*
  Return the child at index using DFS pre-order traversal
  */
 // @param index: The index of the node
-// @return an ICNode at the index
+// @return an ICNode at the index; nil if no such node
 - (ICNode *)getChildAtIndex:(NSInteger)index;
-/* 
+/*
  Return the first child of this ICNode, must be the first ICNode in children array
  */
-// @return the first child of this ICNode. First child == First immediate child
+// @return the first child of this ICNode. First child == First immediate child; nil if no such node
 - (ICNode *)getFirstChild;
-/* 
+/*
  Return the last immediate child of this ICNode, must be the last ICNode in children array
  */
-// @return the last immediate child of this ICNode.
+// @return the last immediate child of this ICNode; nil if no such node
 - (ICNode *)getLastImmediateChild;
 /*
  Return the last child of this ICNode, this is the left-most child of the subtree
  */
-// @return the last child of this ICNode
+// @return the last child of this ICNode; nil if no such node
 - (ICNode *)getLastChild;
+/*
+ Return the node which is the older sibling of this node which means the returned node is also in this node's parent children array but in the location right before this node
+ */
+ // @return The node in this node's parent children whose location is right before this node, if any; nil if no such node
+- (ICNode *)getPreviousSibling;
+- (ICNode *)getNextSibling;
 /*
  Given an ICNode, try to find the index of it in this subtree
  Return -1 if not found
@@ -89,11 +108,11 @@ count
  */
 // @param index: The node at index will become aNode's parent. This index is relative to this ICNode.
 // @param aNode: an ICNode object
-// @return an integer indicates the final location of aNode
+// @return an integer indicates the final location of aNode; -1 if not able to add
 - (NSInteger)addAsChildToIndex:(NSInteger)index withNode:(ICNode *)aNode;
 // @param aParent: The node which will become aNode's parent
 // @param aNode: an ICNode object
-// @return an integer indicates the final location of aNode
+// @return an integer indicates the final location of aNode; -1 if not able to add
 - (NSInteger)addAsChildToNode:(ICNode *)aParent withNode:(ICNode *)aNode;
 /*
  Add the given aNode as a sibling of the node at index.
@@ -101,11 +120,11 @@ count
  */
 // @param index: The node which will be aNode's immediate higher sibling. This index is relative to this ICNode.
 // @param aNode: an ICNode object
-// @return an integer indicates the final location of aNode
+// @return an integer indicates the final location of aNode; -1 if not able to add
 - (NSInteger)addAsSiblingToIndex:(NSInteger)index withNode:(ICNode *)aNode;
 // @param index: The node which will be aNode's immediate higher sibling
 // @param aNode: an ICNode object
-// @return an integer indicates the final location of aNode
+// @return an integer indicates the final location of aNode; -1 if not able to add
 - (NSInteger)addAsSiblingToNode:(NSInteger)targetNode withNode:(ICNode *)aNode;
 
 #pragma mark - remove node from tree
@@ -113,13 +132,13 @@ count
  Remove all children form the node at the given index
  */
 // @param index: the index of the node whose children will be removed from it. This index is relative to this ICNode.
-// @return an integer which indicates how many nodes have been removed from index node
+// @return an integer which indicates how many nodes have been removed from index node; -1 if not able to remove
 - (NSInteger)removeAllChildrenFromIndex:(NSInteger)index;
 /*
  Remove all children including itself
  */
 // @param index: the index of the node whose children will be removed including itself. This index is relative to this ICNode.
-// @return an integer which indicates how many nodes have been removed
+// @return an integer which indicates how many nodes have been removed; -1 if not able to remove
 - (NSInteger)removeNodeAtIndex:(NSInteger)index;
 
 #pragma mark - Moving node
@@ -128,6 +147,7 @@ count
  */
 // @param fromIndex: The index of ICNode which will be moved
 // @param toIndex: The index of ICNode which will be the parent of fromIndex node
+// @return BOOL: whether the moving operation is able to complete or not
 - (BOOL)moveNodeFromIndex:(NSInteger)fromIndex toIndex:(NSInteger)toIndex;
 
 #pragma mark - indentation
@@ -136,5 +156,34 @@ count
 
 #pragma mark - helper methods
 - (BOOL)checkIndexInBound:(NSInteger)index;
-- (BOOL)validateTree;
+/*
+ Traverse the tree in DFS pre-order to see if all nodes have parent
+ */
+//- (BOOL)validateTree;
+/*
+ Return a description of this ICNode but not its children
+ */
+- (NSString *)description;
+/*
+ Return a string which print the entire tree starts from this node just like Linux tree command does. Pring list view
+
+ root
+|-- 1
+|		|-- 2
+|		|		`-- 9
+|		`-- 3
+|-- 4
+|		|-- 5
+|		|		`-- 10
+|		|				`-- 11
+|		|-- 6
+|		`-- 7
+`-- 8
+ */
+- (NSString *)printTree;
+/*
+ Flatten the tree to array view and return it
+ */
+// @return an NSArray which represents the array view of this tree
+- (NSArray *)flatThisNode;
 @end
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ICNode
 
-`ICNode` is designed to represent n ordered n-ary generic tree data structure written in objective-c. The basic idea is to write a data structure which possesses
+`ICNode` is designed to represent an ordered n-ary generic tree data structure written in objective-c where the main idea is to have hierarchical data structure with easy array-like access. ICNode possesses
 
 * The ability to be manupulated easily like a tree
 * The ability to be presented like an array so that developer can use it as a backend data source for a `TableView` very easily
@@ -12,3 +12,56 @@
 * To mimic array-like functionality, all the traversal operation in ICNode is using <a href="http://en.wikipedia.org/wiki/Tree_traversal#Example">DFS pre-order traversal</a>
 * ICNode should have operations like adding child, moving child, deleting child to be performed like a tree by manipulating the sub-nodes around.
 * ICNode should have operations like querying, finding, sorting to be performed like an array
+
+## Illustration of ICNode
+
+<p>Some diagrams <a href="http://ordinarygeek.me/icnode/">here</a></p>
+
+* Tree view:
+					root
+				 /   | \
+			  1    4  8
+			 / \ / | \
+			2  3 5 6 7
+		 /    /
+		9    10
+				 /
+				11
+* List view:
+root
+|-- 1
+|		|-- 2
+|		|		`-- 9
+|		`-- 3
+|-- 4
+|		|-- 5
+|		|		`-- 10
+|		|				`-- 11
+|		|-- 6
+|		`-- 7
+`-- 8
+
+* Array view:
+
+root
+1
+2
+9
+3
+4
+5
+10
+11
+6
+7
+8
+
+## Advantages
+
+* Because each ICNode can be treated like a sub-tree, it is easy to move nodes around, delete nodes and all its children and etc.
+* In addition to being an array, being a tree represents the hierarchical concept of the data.
+* Same time complexity for traversing the tree as searching an array
+
+## Disadvantages
+
+* Since each ICNode(or we can say an array element) holds reference to its parent and children, space complexity increases.
